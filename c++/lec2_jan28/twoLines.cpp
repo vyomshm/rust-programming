9b10c2be1a370330e6b2e761e8f4c4f1988d1cb4
Vyom Sharma <vyomshm@gmail.com>
2019-01-31 01:50:06 +0530

    rust guessing game + c++ lec2

--- c++/lec2_jan28/twoLines.cpp
+++ c++/lec2_jan28/twoLines.cpp
@@ -13,15 +13,25 @@ int main(int argc, char** argv){
 	cout << "Enter x3 and x4: \n";
 	cin >> x3 >> x4;
 
-	if(x1 < x2){
+	if(x1 > x2){
 		d = x2;
 		x2 = x1;
 		x1 = d; 
 	}
 
-	if(x3 < x4){
+	if(x3 > x4){
 		d = x4;
 		x4 = x3;
 		x3 = d;
+	}
+
+	if(x1 >= x4 || x3 >= x2){
+		cout << "No Overlap" << endl;
+	} else if(x1 <= x3 && x2 >= x4){
+		cout << "L2 is within L1" << endl;
+	} else if(x3 <= x1 && x4 >= x2){
+		cout << "L1 is within L2" << endl;
+	} else {
+		cout << "L1 and L2 are overlapping lines" << endl;
 	}
 }
--- cargo-rust-projects/guessing_game/target/debug/deps/guessing_game-c1daacdba877defe.d
+++
@@ -1,5 +0,0 @@
-/Users/vcrizpy/Desktop/algoDS/rust-programming/cargo-rust-projects/guessing_game/target/debug/deps/guessing_game-c1daacdba877defe: src/main.rs
-
-/Users/vcrizpy/Desktop/algoDS/rust-programming/cargo-rust-projects/guessing_game/target/debug/deps/guessing_game-c1daacdba877defe.d: src/main.rs
-
-src/main.rs:
